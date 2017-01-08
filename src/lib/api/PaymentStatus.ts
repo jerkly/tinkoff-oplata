a7ef13b9c2109d56b2a64f0d91f32c26a9c0f93e
Nikolay Martynenko <martynenko.nikolay@gmail.com>
2017-01-08 05:40:27 +0300

    Payment status type changed

--- src/lib/api/PaymentStatus.ts
+++ src/lib/api/PaymentStatus.ts
@@ -1,19 +1,21 @@
-export enum PaymentStatus {
-    NEW = <any> 'NEW',
-    CANCELED = <any> 'CANCELED',
-    PREAUTHORIZING = <any> 'PREAUTHORIZING',
-    FORMSHOWED = <any> 'FORMSHOWED',
-    AUTHORIZING = <any> 'AUTHORIZING',
-    THREE_DS_CHECKING = <any> '3DS_CHECKING',
-    THREE_DS_CHECKED = <any> '3DS_CHECKED',
-    AUTHORIZED = <any> 'AUTHORIZED',
-    REVERSING = <any> 'REVERSING',
-    REVERSED = <any> 'REVERSED',
-    CONFIRMING = <any> 'CONFIRMING',
-    CONFIRMED = <any> 'CONFIRMED',
-    REFUNDING = <any> 'REFUNDING',
-    REFUNDED = <any> 'REFUNDED',
-    PARTIAL_REFUNDED = <any> 'PARTIAL_REFUNDED',
-    REJECTED = <any> 'REJECTED',
-    UNKNOWN = <any> 'UNKNOWN'
+export type PaymentStatus = 'NEW' | 'CANCELED' | 'PREAUTHORIZING' | 'FORMSHOWED' | 'AUTHORIZING' | '3DS_CHECKING' | '3DS_CHECKED' | 'AUTHORIZED' | 'REVERSING' | 'REVERSED' | 'CONFIRMING' | 'CONFIRMED' | 'REFUNDING' | 'REFUNDED' | 'PARTIAL_REFUNDED' | 'REJECTED' | 'UNKNOWN'
+
+export namespace PaymentStatus {
+    export const NEW: PaymentStatus = 'NEW';
+    export const CANCELED: PaymentStatus = 'CANCELED';
+    export const PREAUTHORIZING: PaymentStatus = 'PREAUTHORIZING';
+    export const FORMSHOWED: PaymentStatus = 'FORMSHOWED';
+    export const AUTHORIZING: PaymentStatus = 'AUTHORIZING';
+    export const THREE_DS_CHECKING: PaymentStatus = '3DS_CHECKING';
+    export const THREE_DS_CHECKED: PaymentStatus = '3DS_CHECKED';
+    export const AUTHORIZED: PaymentStatus = 'AUTHORIZED';
+    export const REVERSING: PaymentStatus = 'REVERSING';
+    export const REVERSED: PaymentStatus = 'REVERSED';
+    export const CONFIRMING: PaymentStatus = 'CONFIRMING';
+    export const CONFIRMED: PaymentStatus = 'CONFIRMED';
+    export const REFUNDING: PaymentStatus = 'REFUNDING';
+    export const REFUNDED: PaymentStatus = 'REFUNDED';
+    export const PARTIAL_REFUNDED: PaymentStatus = 'PARTIAL_REFUNDED';
+    export const REJECTED: PaymentStatus = 'REJECTED';
+    export const UNKNOWN: PaymentStatus = 'UNKNOWN';
 }
